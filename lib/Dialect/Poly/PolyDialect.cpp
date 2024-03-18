955f45d4a12198e556e9aaa5ecb28bd948dd97a1
Brian Rodriguez <brod4910@gmail.com>
2024-03-18 14:27:56 -0600

    First Add operation for polynomials

--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,5 +1,5 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
-
+#include "lib/Dialect/Poly/PolyOps.h"
 #include "lib/Dialect/Poly/PolyTypes.h"
 #include "mlir/IR/Builders.h"
 #include "llvm/ADT/TypeSwitch.h"
@@ -7,6 +7,8 @@
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
 
 namespace mlir::toy::poly {
 void PolyDialect::initialize() {
@@ -14,6 +16,10 @@ void PolyDialect::initialize() {
   addTypes<
 #define GET_TYPEDEF_LIST
 #include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+      >();
+  addOperations<
+#define GET_OP_LIST
+#include "lib/Dialect/Poly/PolyOps.cpp.inc"
       >();
 }
 } // namespace mlir::toy::poly
